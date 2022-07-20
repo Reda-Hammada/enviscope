eed64d7b2d645e0b2fddbdfa6a29d5aeac89033b
Reda Hammada <hammada.reda.dev@gmail.com>
2022-07-20 19:37:08 +0200

    update

---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+
+function Header(){
+
+    const logo = <h1>Logohahaha</h1>;
+
+    if(logo.click){
+
+
+
+    };
+    
+    return (
+
+       
+        <header>
+            <nav>
+                <div>
+
+                    <h1>logo</h1>
+
+                </div>
+                <div>
+                    <ul>
+                        <li>Home</li>
+                        <li>Nos services</li>
+                        <li>Nos references</li>
+                        <li>Contact</li>
+                        <li>About</li>
+                    </ul>
+                </div>
+
+            </nav>
+            
+        </header>
+
+    );
+}
+
+export default Header;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Hero (){
+
+    return (
+
+        <main>
+            <section className="ultimate_container">
+
+            </section>
+        </main>
+
+
+    );
+
+}
+
+export default Hero;
