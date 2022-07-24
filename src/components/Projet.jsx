040b160e121ddb6b9e7291217bff993240e4802c
Reda Hammada <hammada.reda.dev@gmail.com>
2022-07-24 21:22:22 +0200

    projet en cours component finished

--- src/components/Projet.jsx
+++ src/components/Projet.jsx
@@ -1,21 +1,59 @@
 import React,{Component}  from 'react';
 import { Fragment } from 'react';
 import Header from './Header';
+import Footer from './Footer';
 
 
 class Projet extends Component { 
 
 
     render () {
-        const data = require('../references/projet.json');
-        console.log(data);
-        return (
+        const dataContainer = {
 
+            width:"70%",
+            backgroundColor:"whiteSmoke",
+            padding:"2% 4%",
+            marginLeft:"15%",
+            marginTop:"2%",
+            
+            fontSize:"1.2rem"
+
+        }
+
+        const ultimateContainer  = {
+
+            marginBottom:"5%",
+        }
+
+        const heading = {
+
+                marginTop:"3%",
+                fontSize:"2rem",
+                color:"#002060",
+                textAlign:"center",
+                width:"100%",
+        }
+        const data = require('../references/projet.json');
+        return (
+            
             <Fragment> 
                 <Header />
-                <section className="ultimateContainer">
+                <section style={ultimateContainer}>
+                    <h1 style={heading}>Projets en cours</h1>
+                       
+                        <div >
+                        {data.map((projet) =>(
+                            <div style={dataContainer}>
+                                <p>{projet}</p>
 
+                            </div>
+                                  ))}
+                        </div>
+
+
+                  
                 </section>
+                <Footer />
             </Fragment>
         )
     }
